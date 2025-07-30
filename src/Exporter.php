2e3f656a3a5d5ff5b14e2acba50242118caabb7b
Merel Jossart <merel.jossart@outlook.com>
2025-07-30 13:31:53 +0200

    Fix cs

--- src/Exporter.php
+++ src/Exporter.php
@@ -83,7 +83,7 @@ class Exporter extends Plugin
         }
 
         // Defer most setup tasks until Craft is fully initialized
-        Craft::$app->onInit(function () {
+        Craft::$app->onInit(function() {
             Sprig::bootstrap();
             $this->registerElementTypes();
             $this->attachEventHandlers();
@@ -150,7 +150,7 @@ class Exporter extends Plugin
         Event::on(
             Elements::class,
             Elements::EVENT_REGISTER_ELEMENT_TYPES,
-            function (RegisterComponentTypesEvent $event) {
+            function(RegisterComponentTypesEvent $event) {
                 $event->types[] = ExportElement::class;
             }
         );
@@ -161,7 +161,7 @@ class Exporter extends Plugin
         Event::on(
             UrlManager::class,
             UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
+            function(RegisterUrlRulesEvent $event) {
                 $event->rules['exporter'] = 'exporter/element/index';
                 $event->rules['exporter/create'] = 'exporter/element/edit';
                 $event->rules['exporter/<elementId:\\d+>/<step:\\d+>'] = 'exporter/element/edit';
@@ -178,7 +178,7 @@ class Exporter extends Plugin
         Event::on(
             ElementTypeHelper::class,
             ElementTypeHelper::EVENT_REGISTER_EXPORTABLE_ELEMENT_TYPES,
-            function (RegisterExportableElementTypes $event) {
+            function(RegisterExportableElementTypes $event) {
                 $entryModel = new ExportableEntryModel();
                 $categoryModel = new ExportableCategoryModel();
                 $userModel = new ExportableUserModel();
@@ -196,7 +196,7 @@ class Exporter extends Plugin
         Event::on(
             Gc::class,
             Gc::EVENT_RUN,
-            function (Event $event) {
+            function(Event $event) {
                 // Delete `elements` table rows without peers in our custom products table
                 Craft::$app->getGc()->deletePartialElements(
                     ExportElement::class,
@@ -209,7 +209,7 @@ class Exporter extends Plugin
         Event::on(
             CraftVariable::class,
             CraftVariable::EVENT_DEFINE_BEHAVIORS,
-            function (DefineBehaviorsEvent $e) {
+            function(DefineBehaviorsEvent $e) {
                 $e->sender->attachBehaviors([
                     CraftVariableBehavior::class,
                 ]);
@@ -219,7 +219,7 @@ class Exporter extends Plugin
         Event::on(
             CraftVariable::class,
             CraftVariable::EVENT_INIT,
-            function (Event $event) {
+            function(Event $event) {
                 /** @var CraftVariable $variable */
                 $variable = $event->sender;
                 $variable->set('exporter', ExporterVariable::class);
@@ -229,7 +229,7 @@ class Exporter extends Plugin
 
     private function registerUserPermissions()
     {
-        Event::on(UserPermissions::class, UserPermissions::EVENT_REGISTER_PERMISSIONS, function (RegisterUserPermissionsEvent $event) {
+        Event::on(UserPermissions::class, UserPermissions::EVENT_REGISTER_PERMISSIONS, function(RegisterUserPermissionsEvent $event) {
             $event->permissions[] = [
                 'heading' => Craft::t('exporter', 'Exporter'),
                 'permissions' => [
@@ -246,7 +246,7 @@ class Exporter extends Plugin
             Event::on(
                 FieldTypeHelper::class,
                 FieldTypeHelper::EVENT_REGISTER_EXPORTABLE_FIELD_TYPES,
-                function (RegisterExportableFieldTypes $event) {
+                function(RegisterExportableFieldTypes $event) {
                     $parsers = $event->fieldTypes;
                     $parsers[PlainTextParser::class][] = \craft\redactor\Field::class; // @phpstan-ignore-line
                     $event->fieldTypes = $parsers;
@@ -261,7 +261,7 @@ class Exporter extends Plugin
             Event::on(
                 FieldTypeHelper::class,
                 FieldTypeHelper::EVENT_REGISTER_EXPORTABLE_FIELD_TYPES,
-                function (RegisterExportableFieldTypes $event) {
+                function(RegisterExportableFieldTypes $event) {
                     $parsers = $event->fieldTypes;
                     $parsers[PlainTextParser::class][] = \craft\ckeditor\Field::class; // @phpstan-ignore-line
                     $event->fieldTypes = $parsers;
@@ -278,7 +278,7 @@ class Exporter extends Plugin
             Event::on(
                 ElementTypeHelper::class,
                 ElementTypeHelper::EVENT_REGISTER_EXPORTABLE_ELEMENT_TYPES,
-                function (RegisterExportableElementTypes $event) {
+                function(RegisterExportableElementTypes $event) {
                     $model = new ExportableFormieSubmissionModel();
                     $event->elementTypes = array_merge($event->elementTypes, [
                         /** @phpstan-ignore-next-line */
@@ -290,7 +290,7 @@ class Exporter extends Plugin
             Event::on(
                 FieldTypeHelper::class,
                 FieldTypeHelper::EVENT_REGISTER_EXPORTABLE_FIELD_TYPES,
-                function (RegisterExportableFieldTypes $event) {
+                function(RegisterExportableFieldTypes $event) {
                     $parsers = $event->fieldTypes;
 
                     $event->fieldTypes[PlainTextParser::class] = array_merge($parsers[PlainTextParser::class], [
--- src/elements/ExportElement.php
+++ src/elements/ExportElement.php
@@ -4,11 +4,11 @@ namespace studioespresso\exporter\elements;
 
 use Craft;
 use craft\base\Element;
-use craft\elements\Entry;
 use craft\elements\conditions\ElementConditionInterface;
 use craft\elements\conditions\entries\EntryCondition;
 use craft\elements\db\ElementQuery;
 use craft\elements\db\ElementQueryInterface;
+use craft\elements\Entry;
 use craft\elements\User;
 use craft\helpers\Db;
 use craft\helpers\Json;
@@ -235,7 +235,7 @@ class ExportElement extends Element
 
     public function getSelectedFields(): array
     {
-        return array_filter($this->getFields(), function ($field) {
+        return array_filter($this->getFields(), function($field) {
             if ($field['handle']) {
                 return true;
             }
@@ -248,7 +248,7 @@ class ExportElement extends Element
     {
         $elementSettings = Exporter::getInstance()->elements->getElementTypeSettings($this->elementType);
         $settings = $this->getSettings();
-        $group = array_filter($elementSettings['group']['items'], function ($group) use ($settings) {
+        $group = array_filter($elementSettings['group']['items'], function($group) use ($settings) {
             if ($group->id == $settings['group']) {
                 return true;
             }
